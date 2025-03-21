827ce154c13204f0d1fe23c0e3f019e990717290
Juan Cochia <juanjo90c@gmail.com>
2025-03-21 00:11:10 +0000

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
-var app = express();
+const app = express();
+const multer = require('multer')
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) =>{
+  res.json({
+    'name' : req.file.originalname,
+    'type' : req.file.mimetype,
+    'size' : req.file.size
+  })
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
